32d2eff7eedc599ce1f861cfe4f7d76d84e71479
miyeong75 <pmy63096276@gmail.com>
2020-12-13 23:51:00 +0900

    added

--- header.h
+++ header.h
@@ -1,3 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct user {
+    int period;
+    int num_people;
+} user_t;
+
+typedef struct schedule {
+    int day;
+    char place[40];
+} schedule_t;
+
+typedef struct node_1 {
+    schedule_t s;
+    struct node_1* next;
+} node_s;
+
+typedef struct info {
+    char place[40];
+    int admission;
+} info_t;
+
+typedef struct node_2 {
+    info_t i;
+    struct node_2* next;
+} node_i;
+
--- main.c
+++ main.c
@@ -1,32 +1,5 @@
 #include "header.h"
 
-typedef struct user {
-    int period;
-    int num_people;
-} user_t;
-
-user_t u1;
-
-typedef struct schedule {
-    int day;
-    char place[40];
-} schedule_t;
-
-typedef struct node_1 {
-    schedule_t s;
-    struct node_1* next;
-} node_s;
-
-typedef struct info {
-    char place[40];
-    int admission;
-} info_t;
-
-typedef struct node_2 {
-    info_t i;
-    struct node_2* next;
-} node_i;
-
 void userInfo(void);
 void printSchedule(void);
 void makeSchedule(void);
@@ -34,6 +7,7 @@ void chPlace(void);
 //void chDay(void);
 void reset(void);
 
+user_t u1;
 
 void main(void) {
 	
